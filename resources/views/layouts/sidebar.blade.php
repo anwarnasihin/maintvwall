ad6efd24833f216d76e5ffbd22cbbed2b5d6548c
Anwar Nasihin <anwarnasihin555@gmail.com>
2023-08-18 09:42:44 +0700

    upp

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,17 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{asset ('assets/index3.html')}}" class="brand-link">
+    {{-- <a href="{{asset ('assets/index3.html')}}" class="brand-link">
       <img src="{{asset ('')}}assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+      <span class="brand-text font-weight-light">kuigiufiuu 3</span>
+    </a> --}}
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset ('assets/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset ('assets/dist/img/jajang.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -24,10 +24,19 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="/tvwall">
+            <a href="/tvwall" class="nav-link">
               <i class="nav-icon fa fa-desktop"></i>
               <p>
-                DISPLAY
+                Display
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-power-off"></i>
+              <p>
+                Logout
               </p>
             </a>
           </li>
