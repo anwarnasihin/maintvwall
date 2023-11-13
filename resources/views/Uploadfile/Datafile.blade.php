12068bb378b3aa9a93ff00b98e614507c4b2cc2f
Anwar Nasihin <anwarnasihin555@gmail.com>
2023-11-13 09:50:45 +0700

    update

--- resources/views/Uploadfile/Datafile.blade.php
+++ resources/views/Uploadfile/Datafile.blade.php
@@ -93,6 +93,34 @@
         <!-- /.card -->
       </div>
 
+      {{-- new --}}
+                {{-- Modal Delete --}}
+                {{-- <div class="modal fade" id="modal-hapus{{$item->id}}">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title">Konfirmasi Hapus Data</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <p>Apakah kamu yakin ingin menghapus data <b>{{ $item->name}}</b> </p>
+                      </div>
+                      <div class="modal-footer justify-content-between">
+                        <form action="{{ route('deletefile',['id' => $item->id]) }}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <button type="button" class="btn btn-primary ml-auto" data-dismiss="modal">Close</button>
+                          <button type="submit" class="btn btn-danger">Ya, Hapus</button>
+                        </form>
+                      </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                </div> --}}
+       {{-- end new --}}
+
 
       <!-- Modal edit -->
       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
