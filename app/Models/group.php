e577e52731dd1030e0fc41016329e95d1025bf07
Anwar Nasihin <anwarnasihin555@gmail.com>
2025-08-23 21:49:49 +0700

    up error dashboard 2

--- app/Models/group.php
+++ app/Models/group.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class group extends Model
+class Group extends Model
 {
     use HasFactory, SoftDeletes;
 
--- app/Models/text.php
+++ app/Models/text.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 // use Illuminate\Database\Eloquent\SoftDeletes;
 
-class text extends Model
+class Text extends Model
 {
     protected $fillable = [
         'judul',
