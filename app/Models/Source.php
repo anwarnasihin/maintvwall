22ca820268fc039af229171974383eb1061e1c60
Anwar Nasihin <anwarnasihin555@gmail.com>
2025-08-23 23:59:04 +0700

    up user and group

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -5,30 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Group;
+
 use App\Models\User;
+use App\Models\Group;
 
 class Source extends Model
 {
     use HasFactory, SoftDeletes;
-
     protected $fillable = [
-        'group_id',
+        'group',
         'typeFile',
         'direktori',
         'duration',
         'str_date',
         'ed_date',
-        'user_id'
+        'users'
     ];
 
-    public function group()
+    public function groups()
     {
-        return $this->belongsTo(Group::class, 'group_id', 'id');
+        return $this->belongsTo(group::class, 'group', 'id');
     }
-
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class, 'users', 'id');
     }
 }
