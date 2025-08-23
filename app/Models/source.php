73d9b74e3d0536badc71d309c17db67f901b22f9
Anwar Nasihin <anwarnasihin555@gmail.com>
2025-08-23 23:25:35 +0700

    error display3

--- app/Models/source.php
+++ app/Models/source.php
@@ -5,26 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Group;
+use App\Models\User;
 
 class Source extends Model
 {
     use HasFactory, SoftDeletes;
+
     protected $fillable = [
-        'group',
+        'group_id',
         'typeFile',
         'direktori',
         'duration',
         'str_date',
         'ed_date',
-        'users'
+        'user_id'
     ];
 
-    public function groups()
+    public function group()
     {
-        return $this->belongsTo(Group::class, 'group', 'id');
+        return $this->belongsTo(Group::class, 'group_id', 'id');
     }
+
     public function user()
     {
-        return $this->belongsTo(User::class, 'users', 'id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
